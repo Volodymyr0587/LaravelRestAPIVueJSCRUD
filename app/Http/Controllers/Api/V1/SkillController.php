f9254d25627cda968781fdb5d08922088c3d1611
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-02 11:39:47 +0300

    04 - Store method

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
+
 
 class SkillController extends Controller
 {
     public function index()
     {
         return response()->json("Skill Index");
+    }
+
+    public function store(StoreSkillRequest $request)
+    {
+        Skill::create($request->validated());
+        return response()->json("Skill Created");
     }
 }
